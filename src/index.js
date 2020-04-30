1fa4077a96f92ae653ff665976abcd30920b79fa
RL Brown <rich@pineandvine.com>
2020-04-30 13:39:21 -0400

    tweaks

--- src/index.js
+++ src/index.js
@@ -39,7 +39,7 @@ const markov = (text) =>
 
 // from the docs' "Listener Middleware" section
 async function noBotMessages({ message, next }) {
-  console.log(message);
+  // console.log(message);
   /*
    * https://github.com/slackapi/bolt/issues/465
    *
@@ -72,15 +72,22 @@ app.message(noBotMessages, iHeardHoward, async ({ say, message }) => {
   try {
     const { channel } = message;
     if (channel === "C61L2R7N2") {
-      console.log("debug channel - always responding");
+      console.log("debug channel - always responding with markov");
       const quote = await markov(message.text);
       await say(quote);
     } else if (channel === "C3ZHJ4K9Q") {
-      console.log("testing channel, always responding random");
-      const quote = await randomQuote();
-      await say(quote);
-    } else if (coinflip(50)) {
-      const quote = await randomQuote();
+      console.log(
+        "testing channel, responding 50% of the time with random quote"
+      );
+      if (coinflip(50)) {
+        const quote = await randomQuote();
+        await say(quote);
+      }
+    } else if (coinflip(25)) {
+      console.log(
+        "not testing or debug. 25% chance of the search-func speaking"
+      );
+      const quote = await search(message.text);
       await say(quote);
     } else {
       console.log("eh, i'll pass.");
