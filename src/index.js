be00388fee34021a20a28ac4006e22f1a6c8a524
RL Brown <rich@pineandvine.com>
2020-04-30 13:22:48 -0400

    cleanup

--- src/index.js
+++ src/index.js
@@ -90,59 +90,50 @@ app.message(noBotMessages, iHeardHoward, async ({ say, message }) => {
   }
 });
 
-app.event('app_home_opened', async ({ event, context }) => {
-  console.log("ahoy")
+app.event("app_home_opened", async ({ event, context }) => {
+  console.log("ahoy");
   try {
     /* view.publish is the method that your app uses to push a view to the Home tab */
     const result = await app.client.views.publish({
-
-      /* retrieves your xoxb token from context */
       token: context.botToken,
-
-      /* the user that opened your app's app home */
       user_id: event.user,
-
-      /* the view payload that appears in the app home*/
       view: {
-        type: 'home',
-        callback_id: 'home_view',
-
-        /* body of the view */
+        type: "home",
+        callback_id: "home_view",
         blocks: [
           {
-            "type": "section",
-            "text": {
-              "type": "mrkdwn",
-              "text": "*Welcome to your _App's Home_* :tada:"
-            }
+            type: "section",
+            text: {
+              type: "mrkdwn",
+              text: "*Welcome to your _App's Home_* :tada:",
+            },
           },
           {
-            "type": "divider"
+            type: "divider",
           },
           {
-            "type": "section",
-            "text": {
-              "type": "mrkdwn",
-              "text": "This button won't do much for now but you can set up a listener for it using the `actions()` method and passing its unique `action_id`. See an example in the `examples` folder within your Bolt app."
-            }
+            type: "section",
+            text: {
+              type: "mrkdwn",
+              text: "This button won't do much.",
+            },
           },
           {
-            "type": "actions",
-            "elements": [
+            type: "actions",
+            elements: [
               {
-                "type": "button",
-                "text": {
-                  "type": "plain_text",
-                  "text": "Click me!"
-                }
-              }
-            ]
-          }
-        ]
-      }
+                type: "button",
+                text: {
+                  type: "plain_text",
+                  text: "Click me!",
+                },
+              },
+            ],
+          },
+        ],
+      },
     });
-  }
-  catch (error) {
+  } catch (error) {
     console.error(error);
   }
 });
