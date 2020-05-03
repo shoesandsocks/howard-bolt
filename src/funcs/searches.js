1c687ac445b7900bfa21a4b4fd27b0b6dc01e619
RL Brown <rich@pineandvine.com>
2020-05-03 07:20:45 -0400

    refac

--- src/funcs/searches.js
+++ src/funcs/searches.js
@@ -5,38 +5,39 @@ import { howard } from "./howard";
 const rnd = (arr) => Math.floor(Math.random() * arr.length);
 const url = "https://howardchicken.online/howard";
 
-const getQuote = () =>
-  fetch(url, {
+const getHowardsReply = async ({ query, argument }) => {
+  const x = await fetch(url, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
-    body: JSON.stringify({
-      query: "getQuote",
-      argument: 1,
-    }),
+    body: JSON.stringify({ query, argument }),
   });
+  console.log(x);
+  return x;
+};
 
 export const randomQuote = () =>
-  getQuote()
+  getHowardsReply("getQuote", 1)
     .then(({ howardsReply }) => howardsReply)
     .catch(() => "fartz");
 
-export const search = (textToSearch) =>
-  howard("searchQuotes", textToSearch)
-    .then(({ howardsReply }) => {
-      return Array.isArray(howardsReply) && howardsReply.length > 0
-        ? howardsReply[rnd(howardsReply)].text
-        : randomQuote();
-    })
-    .catch(() => randomQuote());
-
 export const markov = (text) =>
-  howard("getMarkov", text)
+  getHowardsReply("getMarkov", text)
     .then(({ howardsReply }) => howardsReply)
     .catch(() => randomQuote());
 
 export const poetize = (text) =>
-  howard("getPoem", text)
+  getHowardsReply("getPoem", text)
     .then(({ howardsReply }) => howardsReply)
+    .catch(() => randomQuote());
+
+export const search = (textToSearch) =>
+  getHowardsReply("searchQuotes", textToSearch)
+    .then(({ howardsReply }) => howardsReply)
+    .then((reply) =>
+      Array.isArray(reply) && reply.length > 0
+        ? reply[rnd(reply)].text
+        : randomQuote()
+    )
     .catch(() => randomQuote());
