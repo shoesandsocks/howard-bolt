91bb0f091aebcc29b978ba67ce2e799249164ae7
RL Brown <rich@pineandvine.com>
2020-05-03 17:42:46 -0400

    refactor markov to train once, not repeatedly

--- src/funcs/markov.js
+++ src/funcs/markov.js
@@ -4,13 +4,17 @@ import { getHowardsReply } from "./searches";
 
 const markov = new Markov();
 
-export default async () => {
+const setup = async () => {
   const texties = await getHowardsReply({
     query: "getAll",
     argument: "really",
   }).then((howardsReply) => howardsReply.map((h) => h.original.text));
-  console.log(texties);
   markov.addStates(texties);
   markov.train(3);
+};
+
+setup();
+
+export default async () => {
   return markov.generateRandom(50);
 };
