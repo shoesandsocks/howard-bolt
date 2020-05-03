0c76ec931eb00750c64f7904317997dfef692879
RL Brown <rich@pineandvine.com>
2020-05-03 12:33:41 -0400

    add log

--- src/command-listener.js
+++ src/command-listener.js
@@ -4,6 +4,7 @@ const listenToCommands = (app) => {
   app.command("/howard", async ({ command, ack, say }) => {
     await ack();
     const response = await runCoreUpdate();
+    console.log(response);
     await say(response);
   });
 };
