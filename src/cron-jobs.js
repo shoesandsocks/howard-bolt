37f371bdf91c888eb431d67bc6129368cfb57af4
RL Brown <rich@pineandvine.com>
2020-10-17 15:30:48 -0400

    turn off DanielAsAService call

--- src/cron-jobs.js
+++ src/cron-jobs.js
@@ -2,7 +2,7 @@ import cron from "node-cron";
 
 import { getHowardsReply } from "./funcs/searches";
 import store from "./store";
-import makeMP3 from "./funcs/phraseToMP3";
+// import makeMP3 from "./funcs/phraseToMP3";
 
 require("dotenv").config();
 
@@ -18,22 +18,25 @@ const runJobs = (app) => {
   });
 
   // temporary dev job
-  cron.schedule("0 13 * * 5", async () => {
-    const quote = await getHowardsReply({ query: "getQuotes", argument: 1 });
-    return makeMP3(quote)
-      .then((reply) => reply.json())
-      .then(async (json) => {
-        if (json.error) {
-          return console.log(json.error);
-        }
-        const postIt = await app.client.chat.postMessage({
-          token: process.env.SLACK_BOT_TOKEN,
-          channel: store.getChannel(),
-          text: json.response.filename,
-        });
-      })
-      .catch((e) => console.log(e));
-  });
+  // FIXME: makeMP3 doesn't work unless I run ngrok on iMac
+  // turning this off for now, until I can come up with
+  // a Mac in the kloüd, to run DanielAsAService there
+  // cron.schedule("0 13 * * 5", async () => {
+  //   const quote = await getHowardsReply({ query: "getQuotes", argument: 1 });
+  //   return makeMP3(quote)
+  //     .then((reply) => reply.json())
+  //     .then(async (json) => {
+  //       if (json.error) {
+  //         return console.log(json.error);
+  //       }
+  //       const postIt = await app.client.chat.postMessage({
+  //         token: process.env.SLACK_BOT_TOKEN,
+  //         channel: store.getChannel(),
+  //         text: json.response.filename,
+  //       });
+  //     })
+  //     .catch((e) => console.log(e));
+  // });
 };
 
 export default runJobs;
