917024bcc617183bb3c7fc63fd62d9a45123b6d8
RL Brown <rich@pineandvine.com>
2021-03-31 16:15:17 -0400

    add new log to see what's failing in .match()

--- src/message-listener.js
+++ src/message-listener.js
@@ -7,7 +7,14 @@ async function noBotMessages({ message, next }) {
   }
 }
 async function iHeardHoward({ message, next, say }) {
-  if (message.text.match(/(howard|Howard|howie|Howie|HC|chicken|Chicken)/g)) {
+  console.log(
+    `did I hear my name? this is the text I'm supposed to evaluate: ${message.text}\n(this is an absurd console.log() test)`
+  );
+  if (
+    message &&
+    message.text &&
+    message.text.match(/(howard|Howard|howie|Howie|HC|chicken|Chicken)/g)
+  ) {
     console.log("i heard my name");
     const quote = await getHowardsReply({
       query: queries.getQuotes,
