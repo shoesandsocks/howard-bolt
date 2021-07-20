b11d2ec23c1dfa7c264e2b80e2b8797c0c31d75b
RL Brown <rich@pineandvine.com>
2021-07-20 15:59:35 -0400

    undo

--- src/funcs/config.js
+++ src/funcs/config.js
@@ -1,7 +1,9 @@
-const isProd = process.env.NODE_ENV == "production";
-console.log(isProd);
-const url = isProd
-  ? "http://node-aggregator/howardAPI/"
-  : "https://node.porknachos.com/howardAPI/";
+// const isProd = process.env.NODE_ENV == "production";
+
+// const url = isProd
+//   ? "http://node-aggregator/howardAPI/"
+//   : "https://node.porknachos.com/howardAPI/";
+
+const url = "https://node.porknachos.com/howardAPI/";
 
 export default url;
