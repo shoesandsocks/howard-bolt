b77278e2c997495aebed595c4e9b2f210662832a
RL Brown <rich@pineandvine.com>
2021-07-31 11:54:51 -0400

    markov setup delay of 1 min for server  reasons

--- src/funcs/markov.js
+++ src/funcs/markov.js
@@ -8,12 +8,20 @@ const setup = async () => {
   const texties = await getHowardsReply({
     query: "getAll",
     argument: "really",
-  }).then((howardsReply) => howardsReply.map((h) => h.original.text));
+  })
+    .then((howardsReply) => howardsReply.map((h) => h.original.text))
+    .catch((e) => {
+      console.log(`something died in markov setup`, e);
+    });
   markov.addStates(texties);
   markov.train(3);
 };
 
-setup();
+setTimeout(() => {
+  setup();
+  console.log("okay NOW howard-bolt has prepped the markov thing");
+}, 60000); // give it a minute = in prod this runs before node-aggregator
+// has started, and it fails becuase it can't getHowardsReply yet
 
 export default async () => {
   return markov.generateRandom(50);
