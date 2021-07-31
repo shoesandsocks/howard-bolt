61631ff8024316148f095f776f44d0d6b125d43e
RL Brown <rich@pineandvine.com>
2021-07-31 11:49:32 -0400

    wrap fetch in try-catch

--- src/funcs/searches.js
+++ src/funcs/searches.js
@@ -13,33 +13,39 @@ export const queries = {
   getPoem: "getPoem",
 };
 
-export const getHowardsReply = ({ query, argument }) =>
-  fetch(url, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ query, argument }),
-  })
-    .then((y) => y.json())
-    .then(({ howardsReply }) => {
-      switch (query) {
-        case queries.getEpisode:
-        case queries.getRandomEpisode:
-          return howardsReply.title;
-        case queries.getAll:
-          return argument === "really"
-            ? howardsReply
-            : rnd(howardsReply).original.text;
-        case queries.searchQuotes:
-          return howardsReply.length > 0
-            ? rnd(howardsReply).text
-            : getHowardsReply({ query: queries.getQuotes, argument: 1 });
-        case queries.getQuotes:
-          return argument > 1 ? rnd(howardsReply).text : howardsReply[0].text;
-        case queries.getMarkov:
-        case queries.getPoem:
-        default:
-          return howardsReply;
-      }
-    });
+export const getHowardsReply = ({ query, argument }) => {
+  try {
+    fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ query, argument }),
+    })
+      .then((y) => y.json())
+      .then(({ howardsReply }) => {
+        switch (query) {
+          case queries.getEpisode:
+          case queries.getRandomEpisode:
+            return howardsReply.title;
+          case queries.getAll:
+            return argument === "really"
+              ? howardsReply
+              : rnd(howardsReply).original.text;
+          case queries.searchQuotes:
+            return howardsReply.length > 0
+              ? rnd(howardsReply).text
+              : getHowardsReply({ query: queries.getQuotes, argument: 1 });
+          case queries.getQuotes:
+            return argument > 1 ? rnd(howardsReply).text : howardsReply[0].text;
+          case queries.getMarkov:
+          case queries.getPoem:
+          default:
+            return howardsReply;
+        }
+      });
+  } catch (e) {
+    console.log(`boo.`, e);
+    return "we have uh oh.";
+  }
+};
